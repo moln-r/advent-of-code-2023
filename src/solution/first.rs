3368ab2094643020ad310a3340033ea150e8ff93
Máté Molnár <15122506+moln-r@users.noreply.github.com>
2023-12-01 09:58:01 +0100

    first day first part done

--- src/solution/first.rs
+++ src/solution/first.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 #[derive(Debug)]
 pub struct Trebuchet {}
 
@@ -5,8 +8,35 @@ impl Trebuchet {
     pub(crate) fn solve() -> i32 {
         println!("Solving first days problem");
 
-        
+        let mut sum = 0;
 
-        1
+        let file = File::open("src/solution/input-01")
+            .expect("Error opening file");
+        for line in BufReader::new(file).lines() {
+            if line.is_ok() {
+                let line = line.unwrap();
+
+                let mut num_as_string: String = String::new();
+
+                for c in line.chars() {
+                    if c.is_ascii_digit() {
+                        num_as_string.push(c);
+                        break;
+                    }
+                }
+                for c in line.chars().rev() {
+                    if c.is_ascii_digit() {
+                        num_as_string.push(c);
+                        break;
+                    }
+                }
+                sum += num_as_string.parse::<i32>().unwrap();
+            } else {
+                println!("Error reading a line");
+            }
+        }
+
+        sum
     }
+
 }
