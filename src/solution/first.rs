46071ab880f97abc10d6ebf30ad2d6271ceb474b
Máté Molnár <15122506+moln-r@users.noreply.github.com>
2023-12-01 17:15:50 +0100

    quality improvements

--- src/solution/first.rs
+++ src/solution/first.rs
@@ -16,17 +16,16 @@ impl Trebuchet {
     pub(crate) fn solve(&self) -> i32 {
         println!("Solving first days problem");
 
-        let mut sum = 0;
-
         let file = File::open("src/solution/input-01")
             .expect("Error opening file");
 
+        let mut sum = 0;
         for line in BufReader::new(file).lines() {
-            if line.is_ok() {
+            if line.is_err() {
+                println!("Error reading a line");
+            } else {
                 let line = line.unwrap();
-                println!("Line: {}", line);
-
-                let mut num_as_string: String = String::new();
+                // println!("Line: {}", line);
 
                 let first_number = self.find_first_number(&line);
                 let last_number = self.find_last_number(&line);
@@ -35,71 +34,58 @@ impl Trebuchet {
                     panic!("Error finding a number in line: {}", line);
                 }
 
+                let mut num_as_string: String = String::new();
                 // building the number string
                 num_as_string.push(first_number.unwrap());
                 num_as_string.push(last_number.unwrap());
 
                 // converting the string to a number and adding it to the sum
                 sum += num_as_string.parse::<i32>().unwrap();
-            } else {
-                println!("Error reading a line");
             }
         }
-
         sum
     }
 
     fn find_first_number(&self, line: &String) -> Option<char> {
-        println!("Finding first number in line: {}", line);
-        for (i, c) in line.chars().enumerate() {
-            if c.is_ascii_digit() {
-                println!("It's a digit! {}", c);
-                return Some(c)
-            } else {
-                // get nums that start with char based on length left
-                let possible_nums: Vec<&Num> = self.nums.iter()
-                    .filter(|n| n.first_char == c)
-                    .filter(|n| n.str_length <= line.len() - i)
-                    .collect();
-
-                if !possible_nums.is_empty() {
-                    println!("Possible nums for char {}: {:?}", c, possible_nums);
-                }
-
-                // compare num string with the line's slice
-                for num in possible_nums {
-                    if num.as_string == line[i..i + num.str_length] {
-                        println!("Found a match at index {}: {}", i, num.num);
-                        return Some(char::from_digit(num.num as u32, 10).unwrap());
-                    }
-                }
-            }
-        }
-        None
+        // println!("Finding first number in line: {}", line);
+        return self.find_number(line, WhichPart::First);
     }
 
     fn find_last_number(&self, line: &String) -> Option<char> {
-        println!("Finding last number in line: {}", line);
+        // println!("Finding last number in line: {}", line);
         let reverse_string: String = line.chars().rev().collect();
-        for (i, c) in reverse_string.chars().enumerate() {
+        return self.find_number(&reverse_string, WhichPart::Last);
+    }
+
+    fn find_number(&self, string: &String, which_part: WhichPart) -> Option<char> {
+        for (i, c) in string.chars().enumerate() {
             if c.is_ascii_digit() {
-                println!("It's a digit! {}", c);
+                // println!("It's a digit! {}", c);
                 return Some(c);
             } else {
                 // get nums that start with char based on length left
                 let possible_nums: Vec<&Num> = self.nums.iter()
-                    .filter(|n| n.last_char == c)
-                    .filter(|n| n.str_length <= line.len() - i)
+                    .filter(|n| {
+                        return match which_part {
+                            WhichPart::First => { n.first_char == c }
+                            WhichPart::Last => { n.last_char == c }
+                        };
+                    })
+                    .filter(|n| n.str_length <= string.len() - i)
                     .collect();
 
-                if !possible_nums.is_empty() {
-                    println!("Possible nums for char {}: {:?}", c, possible_nums);
-                }
+                // if !possible_nums.is_empty() {
+                //     println!("Possible nums for char {}: {:?}", c, possible_nums);
+                // }
 
                 // compare num string with the line's slice
                 for num in possible_nums {
-                    if num.as_rev_string == reverse_string[i..i + num.str_length] {
-                        println!("Found a match at index {}: {}", i, num.num);
+                    let string_to_compare: &str = match which_part {
+                        WhichPart::First => { num.as_string.as_str() }
+                        WhichPart::Last => { num.as_rev_string.as_str() }
+                    };
+                    if *string_to_compare == string[i..i + num.str_length] {
+                        // println!("Found a match at index {}: {}", i, num.num);
                         return Some(char::from_digit(num.num as u32, 10).unwrap());
                     }
                 }
@@ -138,3 +124,5 @@ impl Num {
         nums
     }
 }
+
+enum WhichPart { First, Last }
