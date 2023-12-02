c887d091b3f7f5a5f4cee7d16f1a53073361630c
Máté Molnár <15122506+moln-r@users.noreply.github.com>
2023-12-02 11:29:57 +0100

    setting up file for tomorrow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::solution::first::Trebuchet;
 use crate::solution::second::CubeConundrum;
+use crate::solution::third::TheNextOne;
 
 pub mod solution;
 
@@ -11,11 +12,16 @@ pub struct Solution {
 }
 
 trait AdventOfCode {
+    fn new() -> Self;
     fn solve(&self) -> Solution;
 }
 
 fn main() {
     println!("Running Advent of code 2023...");
+    // Solution { day: 1, part_one: 53651, part_two: 53894 }
     println!("{:?}", Trebuchet::new().solve());
+    // Solution { day: 2, part_one: 2207, part_two: 62241 }
     println!("{:?}", CubeConundrum::new().solve());
+    // Solution { day: 3, part_one: ?, part_two: ? }
+    println!("{:?}", TheNextOne::new().solve());
 }
---
+++ src/solution/third.rs
@@ -0,0 +1,17 @@
+use crate::{AdventOfCode, Solution};
+
+pub struct TheNextOne {}
+
+impl AdventOfCode for TheNextOne {
+    fn new() -> Self {
+        TheNextOne {}
+    }
+
+    fn solve(&self) -> Solution {
+        Solution {
+            day: 3,
+            part_one: 0,
+            part_two: 0,
+        }
+    }
+}
