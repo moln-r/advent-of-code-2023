4c88040a95b83f068c97c73fa73cde637104127d
Máté Molnár <15122506+moln-r@users.noreply.github.com>
2023-12-02 11:30:11 +0100

    comments (with some refactor)

--- src/solution/first.rs
+++ src/solution/first.rs
@@ -8,55 +8,59 @@ use crate::{AdventOfCode, Solution};
 #[derive(Debug)]
 pub struct Trebuchet {
     day: i32,
-    nums: Vec<Num>,
+    nums: Vec<Num>, // We store the numbers from one to nine in a special struct to help us resolve the problem
 }
 
 impl AdventOfCode for Trebuchet {
-    fn solve(&self) -> Solution {
-        let file = File::open("src/solution/inputs/input-01")
-            .expect("Error opening file");
-
-        let mut part_one = 0;
-        let mut part_two = 0;
-        for line in BufReader::new(file).lines() {
-            if line.is_err() {
-                println!("Error reading a line");
-            } else {
-                let line = line.unwrap();
-
-                let first_number = self.find_number(&line, WhichPart::First, true).unwrap();
-                let last_number = self.find_number(&(line.chars().rev().collect()), WhichPart::Last, true).unwrap();
-
-                part_one += format!("{}{}", first_number, last_number).parse::<i32>().unwrap();
-
-                let first_number = self.find_number(&line, WhichPart::First, false).unwrap();
-                let last_number = self.find_number(&(line.chars().rev().collect()), WhichPart::Last, false).unwrap();
-
-                part_two += format!("{}{}", first_number, last_number).parse::<i32>().unwrap();
-            }
-        }
-        Solution { day: self.day, part_one, part_two }
-    }
-}
-
-impl Trebuchet {
-    pub fn new() -> Trebuchet {
+    fn new() -> Self {
         Trebuchet {
             day: 1,
             nums: Num::init(),
         }
     }
 
+    fn solve(&self) -> Solution {
+        // Open input file for first day
+        let file = File::open("src/solution/inputs/input-01")
+            .expect("Error opening file");
+
+        let mut part_one = 0;
+        let mut part_two = 0;
+        // Read file line by line
+        for line in BufReader::new(file).lines() {
+            let line = line.unwrap();
+
+            // For the first part, find the first and last numbers, looking for digits only
+            let first_number = self.find_number(&line, WhichPart::First, true).unwrap();
+            let last_number = self.find_number(&(line.chars().rev().collect()), WhichPart::Last, true).unwrap();
+            // Format the numbers as a string, parse it to i32 and add it to the part one sum
+            part_one += format!("{}{}", first_number, last_number).parse::<i32>().unwrap();
+
+            // For the second part, find the first and last numbers, looking for numbers as digit or substring
+            let first_number = self.find_number(&line, WhichPart::First, false).unwrap();
+            let last_number = self.find_number(&(line.chars().rev().collect()), WhichPart::Last, false).unwrap();
+            // Format the numbers as a string, parse it to i32 and add it to the part two sum
+            part_two += format!("{}{}", first_number, last_number).parse::<i32>().unwrap();
+        }
+        Solution { day: self.day, part_one, part_two }
+    }
+}
+
+impl Trebuchet {
     fn find_number(&self, string: &String, which_part: WhichPart, only_digit: bool) -> Option<char> {
+        // Iterate over the string's chars
         for (i, c) in string.chars().enumerate() {
             if c.is_ascii_digit() {
-                // println!("It's a digit! {}", c);
+                // If char is a digit, return it
                 return Some(c);
             } else if !only_digit {
-                // get nums that start with char based on length left
+                // If we look for numbers as digit or substring, run extra logic
+
+                // Filter possible numbers by first or last char and by length based on the rest of the string
                 let possible_nums: Vec<&Num> = self.nums.iter()
                     .filter(|n| {
                         return match which_part {
+                            // Based on which part we are looking for, check if the first or last char matches
                             WhichPart::First => { n.first_char == c }
                             WhichPart::Last => { n.last_char == c }
                         };
@@ -64,14 +68,14 @@ impl Trebuchet {
                     .filter(|n| n.str_length <= string.len() - i)
                     .collect();
 
-                // compare num string with the line's slice
+                // Iterate over possible numbers and try to find a match
                 for num in possible_nums {
                     let string_to_compare: &str = match which_part {
+                        // Based on which part we are looking for, get the right string to compare
                         WhichPart::First => { num.as_string.as_str() }
                         WhichPart::Last => { num.as_rev_string.as_str() }
                     };
                     if *string_to_compare == string[i..i + num.str_length] {
-                        // println!("Found a match at index {}: {}", i, num.num);
                         return Some(char::from_digit(num.num as u32, 10).unwrap());
                     }
                 }
@@ -83,6 +87,7 @@ impl Trebuchet {
 
 #[derive(Debug)]
 struct Num {
+    // We store the number, the first and last char, the number as string and as reversed string, and its string length
     num: i32,
     first_char: char,
     last_char: char,
@@ -93,21 +98,18 @@ struct Num {
 
 impl Num {
     fn init() -> Vec<Num> {
-        let mut nums: Vec<Num> = Vec::new();
-        let str_numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
-        for i in 0..str_numbers.len() {
-            nums.push(Num {
-                num: i as i32 + 1,
-                first_char: str_numbers[i].chars().next().unwrap(),
-                last_char: str_numbers[i].chars().last().unwrap(),
-                as_string: str_numbers[i].to_string(),
-                as_rev_string: str_numbers[i].chars().rev().collect::<String>(),
-                str_length: str_numbers[i].len(),
-            });
-        }
-
-        nums
+        // Initialize the numbers from one to nine and create Num structs
+        ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].iter()
+            .enumerate()
+            .map(|(i, n)|
+                Num {
+                    num: i as i32 + 1,
+                    first_char: n.chars().next().unwrap(),
+                    last_char: n.chars().last().unwrap(),
+                    as_string: n.to_string(),
+                    as_rev_string: n.chars().rev().collect::<String>(),
+                    str_length: n.len(),
+                }).collect()
     }
 }
 
--- src/solution/second.rs
+++ src/solution/second.rs
@@ -8,69 +8,13 @@ use crate::{AdventOfCode, Solution};
 
 pub struct CubeConundrum {
     day: i32,
+    // We store the color limit for the first part of the problem
+    // It cannot be a const because Rust does not (yet) support compile time evaluation of code for HashMap initialization
     color_limit: HashMap<String, i32>,
 }
 
 impl AdventOfCode for CubeConundrum {
-    fn solve(&self) -> Solution {
-        let file = File::open("src/solution/inputs/input-02")
-            .expect("Error opening file");
-
-        let per_color_regex = Regex::new("([0-9]+ [a-z]+)").unwrap();
-        let count_per_color = HashMap::from([
-            ("red".to_string(), 0),
-            ("green".to_string(), 0),
-            ("blue".to_string(), 0),
-        ]);
-
-        let mut part_one = 0;
-        let mut part_two = 0;
-        for line in BufReader::new(file).lines() {
-            if line.is_err() {
-                println!("Error reading a line");
-            } else {
-                let line = line.unwrap();
-
-                let game_num = Self::get_game_number(&line);
-                // println!("Game number: {}", game_num);
-
-                let mut any_impossible = false;
-                let mut max_count_per_color = count_per_color.clone();
-
-                for color_info in per_color_regex.captures_iter(&line) {
-                    let color_info = color_info.get(0).unwrap().as_str();
-                    let split = color_info.split_whitespace().collect::<Vec<&str>>();
-                    let count = split[0].parse::<i32>().unwrap();
-                    let color = split[1];
-
-                    let is_color_possible = count <= *self.color_limit.get(color).expect("Color not found");
-                    // println!("Color [{}] is possible: {}", color_info, is_color_possible);
-
-                    if !is_color_possible && !any_impossible {
-                        any_impossible = true;
-                    }
-
-                    // get max count for color and replace it if current count is bigger
-                    let current_max = max_count_per_color.get(color).unwrap();
-                    if current_max < &count {
-                        max_count_per_color.insert(color.to_string(), count);
-                    }
-                }
-
-                // println!("Max count per color: {:?}", max_count_per_color);
-                part_two += max_count_per_color.values().product::<i32>();
-
-                if !any_impossible {
-                    part_one += game_num;
-                }
-            }
-        }
-        Solution { day: self.day, part_one, part_two }
-    }
-}
-
-impl CubeConundrum {
-    pub fn new() -> CubeConundrum {
+    fn new() -> Self {
         CubeConundrum {
             day: 2,
             color_limit: HashMap::from([
@@ -81,15 +25,78 @@ impl CubeConundrum {
         }
     }
 
+    fn solve(&self) -> Solution {
+        // Open input file for second day
+        let file = File::open("src/solution/inputs/input-02")
+            .expect("Error opening file");
+
+        // Regex to find the color info in the line
+        let per_color_regex = Regex::new("([0-9]+ [a-z]+)").unwrap();
+        // HashMap to store the current count for each color
+        let count_per_color = HashMap::from([
+            ("red".to_string(), 0),
+            ("green".to_string(), 0),
+            ("blue".to_string(), 0),
+        ]);
+
+        let mut part_one = 0;
+        let mut part_two = 0;
+        // Read file line by line
+        for line in BufReader::new(file).lines() {
+            let line = line.unwrap();
+
+            // Parse the game number from the line
+            let game_num = CubeConundrum::get_game_number(&line);
+
+            // Setting up a flag to check if any color had more than the allowed number of cubes for the first part
+            let mut any_impossible = false;
+
+            // Clone the count_per_color HashMap to create a mutable version for the current loop
+            let mut max_count_per_color = count_per_color.clone();
+
+            // Iterate over the color info in the line
+            for color_info in per_color_regex.captures_iter(&line) {
+                let color_info = color_info.get(0).unwrap().as_str();
+                // Split the color info into count and color by whitespace
+                let split = color_info.split_whitespace().collect::<Vec<&str>>();
+                let count = split[0].parse::<i32>().unwrap();
+                let color = split[1];
+
+                // Check if the current count for the color is less than the limit
+                // If not, set the flag, because we'll have to add the game num to the part one sum
+                let is_color_possible = count <= *self.color_limit.get(color).expect("Color not found");
+                // println!("Color [{}] is possible: {}", color_info, is_color_possible);
+                if !is_color_possible && !any_impossible {
+                    any_impossible = true;
+                }
+
+                // Replace the current max count for the color with the current count if it is bigger
+                let current_max = max_count_per_color.get(color).unwrap();
+                if current_max < &count {
+                    max_count_per_color.insert(color.to_string(), count);
+                }
+            }
+            // Calculate the product of the max counts for each color and add it to the part two sum
+            part_two += max_count_per_color.values().product::<i32>();
+            // If there was any color with more than the allowed number of cubes, add the game num to the part one sum
+            if !any_impossible {
+                part_one += game_num;
+            }
+        }
+        Solution { day: self.day, part_one, part_two }
+    }
+}
+
+impl CubeConundrum {
     fn get_game_number(line: &String) -> i32 {
         Regex::new("Game ([0-9]+):")
-            .unwrap()
+            .unwrap() // Regex to find the game number in the line
             .captures(&line)
             .unwrap()
-            .get(1)
+            .get(1)// Get the first capture group
             .unwrap()
             .as_str()
-            .parse::<i32>()
+            .parse::<i32>() // Parse the capture group to i32
             .unwrap()
     }
 }
