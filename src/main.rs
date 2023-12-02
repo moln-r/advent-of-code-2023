0538adc185b771c925abec5a918b1f3790fd7bca
Máté Molnár <15122506+moln-r@users.noreply.github.com>
2023-12-02 07:03:47 +0100

    day2!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::solution::first::Trebuchet;
+use crate::solution::second::CubeConundrum;
 
 pub mod solution;
 
@@ -6,4 +7,7 @@ fn main() {
     println!("Running Advent of code 2023...");
     let trebuchet = Trebuchet::new();
     println!("First day's answer: {}", trebuchet.solve());
+
+    let cube_conundrum = CubeConundrum::new();
+    println!("Second day's answer: {}", cube_conundrum.solve());
 }
---
+++ src/solution/second.rs
@@ -0,0 +1,15 @@
+pub struct CubeConundrum {}
+
+impl CubeConundrum {
+    pub(crate) fn new() -> CubeConundrum {
+        println!("gotta do it");
+        CubeConundrum {}
+    }
+}
+
+impl CubeConundrum {
+    pub(crate) fn solve(&self) -> i32 {
+        println!("but really");
+        0
+    }
+}
